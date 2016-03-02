f465e56e8019e2eac6ab5bf7d0066fc1589fbe9d
Alex Bradley <alexandre.bradley@alcatel-lucent.com>
2016-03-02 09:03:42 -0500

    fixed mistargetting of check_direction, passes challenge input!

--- 254-intermediate/legal_reversi_moves.rb
+++ 254-intermediate/legal_reversi_moves.rb
@@ -1,30 +1,48 @@
+#https://www.reddit.com/r/dailyprogrammer/comments/468pvf/20160217_challenge_254_intermediate_finding_legal/
+
 def find_legal_number(player, boardstate)
 	board = board_as_matrix(boardstate)
-	print_board(board)
 	check_all_positions(player,board)
+	print_board(board)
 end
 
 def check_all_positions (player, board)
 	player == "X" ? opponent = "O" : opponent = "X"
+	count = 0
 
 	board.each_index {|x|
 		board[x].each_index { |y|
 
-			# check if the targetted space is occupied
-			valid = initial_check(board[x][y])
+			# check if the targeted space is occupied
+			initial_valid = initial_check(board[x][y])
+			
+			# assume the move is invalid until we find out otherwise
+			valid = false
 
-			# check in all 8 directions for a valid move
-			if valid
+			
+			if initial_valid
+				# check in all 8 directions for a valid move
+				# this will have an unneeded check for 0,0
 				for dir_x in -1..1
 					for dir_y in -1..1
-						# TODO: need to break out of this double loop if a valid position is found
-						valid = check_direction (board, player, opponent, x, y, dir_x, dir_y)
+						valid = check_direction(board, player, opponent, x, y, dir_x, dir_y)
+
+						# update the cell and count if this was deemed a valid move
+						if valid 
+							board[x][y] = "*"
+							count += 1
+						end
+
+						# placing a token at this position is valid, no need to continue the search
+						break if valid
 					end
+					break if valid
 				end
 			end
-
 		}
 	}
+
+	puts "Total valid moves for player " + player + ": " + count.to_s
 end
 
 def initial_check (character)
@@ -33,38 +51,42 @@ end
 
 def check_direction (board, player, opponent, target_x, target_y, dir_x, dir_y)
 
-	#verify if there is an adjacent opponent piece
-	#ideally, I want to have a loop that checks for a series of opponent tokens in a direction and returns true if it hits an allied token
-	#so for this method, I'll need a starting coordinate and a direction
+	# the following is not very DRY, 
+	# I need a way of +1'ing the directions regardless of their sign. Needs to also handle 0s.
+	# lots of repeated "target_z + dir_z"
 
-	# the following is not very DRY, I need a way of +1'ing the directions regardless of their sign.
-	if board[dir_x][dir_y] == opponent # opponent token found, continue searching in this direction
-		if dir_x < 0 && dir_y < 0
-			return check_direction (board, player, opponent, target_x, target_y, dir_x - 1, dir_y - 1)
-		elsif dir_x < 0 && dir_y > 0
-			return check_direction (board, player, opponent, target_x, target_y, dir_x - 1, dir_y + 1)
-		elsif dir_x > 0 && dir_y < 0
-			return check_direction (board, player, opponent, target_x, target_y, dir_x + 1, dir_y - 1)
-		elsif dir_x > 0 && dir_y > 0
-			return check_direction (board, player, opponent, target_x, target_y, dir_x + 1, dir_y + 1)
-		elsif dir_x == 0 && dir_y > 0
-			return check_direction (board, player, opponent, target_x, target_y, dir_x, dir_y + 1)
-		elsif dir_x == 0 && dir_y < 0
-			return check_direction (board, player, opponent, target_x, target_y, dir_x, dir_y - 1)
-		elsif dir_x > 0 && dir_y == 0
-			return check_direction (board, player, opponent, target_x, target_y, dir_x + 1, dir_y)
-		elsif dir_x < 0 && dir_y == 0
-			return check_direction (board, player, opponent, target_x, target_y, dir_x - 1, dir_y)
-		end
-	elsif board[dir_x][dir_y] == player # found our token
-		if dir_x > 1 || dir_y > 1 || dir_x < -1 || dir_y < -1 # we are NOT adjacent to the checked position, valid!
-			return true
-		else # invalid
+	if board[target_x + dir_x] && board[target_x + dir_x][target_y + dir_y]
+		if board[target_x + dir_x][target_y + dir_y] == opponent # opponent token found, continue searching in this direction
+			if dir_x < 0 && dir_y < 0
+				return check_direction(board, player, opponent, target_x, target_y, dir_x - 1, dir_y - 1)
+			elsif dir_x < 0 && dir_y > 0 
+				return check_direction(board, player, opponent, target_x, target_y, dir_x - 1, dir_y + 1)
+			elsif dir_x > 0 && dir_y < 0
+				return check_direction(board, player, opponent, target_x, target_y, dir_x + 1, dir_y - 1)
+			elsif dir_x > 0 && dir_y > 0
+				return check_direction(board, player, opponent, target_x, target_y, dir_x + 1, dir_y + 1)
+			elsif dir_x == 0 && dir_y > 0
+				return check_direction(board, player, opponent, target_x, target_y, dir_x, dir_y + 1)
+			elsif dir_x == 0 && dir_y < 0
+				return check_direction(board, player, opponent, target_x, target_y, dir_x, dir_y - 1)
+			elsif dir_x > 0 && dir_y == 0
+				return check_direction(board, player, opponent, target_x, target_y, dir_x + 1, dir_y)
+			elsif dir_x < 0 && dir_y == 0
+				return check_direction(board, player, opponent, target_x, target_y, dir_x - 1, dir_y)
+			end
+		elsif board[target_x + dir_x][target_y + dir_y] == player # found our token
+			if dir_x > 1 || dir_y > 1 || dir_x < -1 || dir_y < -1 # we are NOT adjacent to the initial position, valid!
+				#puts "Found legal move for " + player + " at x:" + target_x.to_s + " y:" + target_y.to_s + " from: " + dir_x.to_s + " " + dir_y.to_s
+				return true
+			else # we ARE adjacent to our token, which is not a valid move
+				return false
+			end
+		else # last case is finding another blank space, invalid.
+			# This also covers the out of bounds issue according to http://stackoverflow.com/questions/22647254/ruby-arrays-bounds-and-raising-exceptions
+			# because we'll just return false if we get a nil, need to test to make sure ;)
 			return false
 		end
-	else #last case is finding another blank space, invalid.
-		# This also covers the out of bounds issue according to http://stackoverflow.com/questions/22647254/ruby-arrays-bounds-and-raising-exceptions
-		# because we'll just return false if we get a nil, need to test to make sure ;)
+	else # another out of bounds nil catcher
 		return false
 	end
 end
@@ -80,12 +102,12 @@ end
 
 def print_board(board)
 	#playing around with printing arrays
-	board.each do |row|
-		row.each do |element|
-			print element
-		end
-		puts ""
-	end
+#	board.each do |row|
+#		row.each do |element|
+#			print element
+#		end
+#		puts ""
+#	end
 
 	board.each_index {|x|
 		board[x].each_index {|y|
@@ -95,6 +117,18 @@ def print_board(board)
 	}
 end
 
+# run it!
+find_legal_number("X",
+"--------
+--------
+--------
+---OX---
+---XO---
+--------
+--------
+--------"
+)
+
 
 find_legal_number("O",
 "--------
@@ -103,6 +137,17 @@ find_legal_number("O",
 --XXOX--
 ---XOO--
 ----X---
+--------
+--------"
+)
+
+find_legal_number("X",
+"--------
+--------
+---OX---
+--XXXO--
+--XOO---
+---O----
 --------
 --------"
 )
