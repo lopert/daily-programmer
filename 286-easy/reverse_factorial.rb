96bf946ea142b927ba8dc22f8e4279b4cc316fd2
Alex Bradley <alexandre.bradley@nokia.com>
2016-10-04 08:24:50 -0400

    Add results

--- 286-easy/reverse_factorial.rb
+++ 286-easy/reverse_factorial.rb
@@ -1,11 +1,11 @@
-# https://www.reddit.com/r/dailyprogrammer/comments/55nior/20161003_challenge_286_easy_reverse_factorial/
+# https://www.reddit.com/r/dailyprogrammer/comments/55nior/20161003_challenge_286_easy_reverse_factorial/    
 
 def rev_fact(number)
 	output(number, logic(number))
-end
+end    
 
 def logic(number, divider=1)
-	nd = number/divider
+	nd = number/divider    
 
 	if nd == 1
 		return divider
@@ -14,7 +14,7 @@ def logic(number, divider=1)
 	else
 		logic(nd, divider+1)
 	end
-end
+end    
 
 def output(number, result)
 	if result == -1
@@ -22,10 +22,12 @@ def output(number, result)
 	else
 		puts number.to_s + " = " + result.to_s + "!"
 	end
-end
+end    
 
-rev_fact(120)
-rev_fact(3628800)
-rev_fact(479001600)
-rev_fact(6)
-rev_fact(18)
+rev_fact(120)			# 120 = 5!
+rev_fact(3628800)		# 3628800 = 10!
+rev_fact(479001600)		# 479001600 = 12!
+rev_fact(6)				# 6 = 3!
+rev_fact(18)			# 18 NONE
+rev_fact(0)				# 0 NONE
+rev_fact(1)				# 1 = 1!
