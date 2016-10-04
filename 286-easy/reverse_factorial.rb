2615c7cbdf663cba93ce082c09b00d2fc29c5dbe
Alex Bradley <alexandre.bradley@nokia.com>
2016-10-04 08:12:12 -0400

    286 ruby

---
+++ 286-easy/reverse_factorial.rb
@@ -0,0 +1,31 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/55nior/20161003_challenge_286_easy_reverse_factorial/
+
+def rev_fact(number)
+	output(number, logic(number))
+end
+
+def logic(number, divider=1)
+	nd = number/divider
+
+	if nd == 1
+		return divider
+	elsif nd == 0
+		return -1
+	else
+		logic(nd, divider+1)
+	end
+end
+
+def output(number, result)
+	if result == -1
+		puts number.to_s + " NONE"
+	else
+		puts number.to_s + " = " + result.to_s + "!"
+	end
+end
+
+rev_fact(120)
+rev_fact(3628800)
+rev_fact(479001600)
+rev_fact(6)
+rev_fact(18)
