1f50b4c776e60d11a6052a003e103c523f6cf14e
Alex Bradley <alexandre.bradley@nokia.com>
2016-10-05 08:00:22 -0400

    Fix integer truncation bad case

--- 286-easy/reverse_factorial.rb
+++ 286-easy/reverse_factorial.rb
@@ -1,15 +1,17 @@
 # https://www.reddit.com/r/dailyprogrammer/comments/55nior/20161003_challenge_286_easy_reverse_factorial/    
 
-def rev_fact(input)
+def reverse_factorial(input)
 	output(input, logic(input))
 end    
 
-def logic(number, divider=1)
-	nd = number/divider    
+def logic(number, divider=1.0)
+	nd = number/divider 
 
 	if nd == 1
-		return divider
+		return divider.to_i
 	elsif nd == 0
+		return -1
+	elsif nd % 1 == 0
 		return -1
 	else
 		logic(nd, divider+1)
@@ -24,10 +26,11 @@ def output(input, result)
 	end
 end    
 
-rev_fact(120)			# 120 = 5!
-rev_fact(3628800)		# 3628800 = 10!
-rev_fact(479001600)		# 479001600 = 12!
-rev_fact(6)				# 6 = 3!
-rev_fact(18)			# 18 NONE
-rev_fact(0)				# 0 NONE
-rev_fact(1)				# 1 = 1!
+reverse_factorial(120)				# 120 = 5!
+reverse_factorial(3628800)			# 3628800 = 10!
+reverse_factorial(479001600)		# 479001600 = 12!
+reverse_factorial(6)				# 6 = 3!
+reverse_factorial(18)				# 18 NONE
+reverse_factorial(0)				# 0 NONE
+reverse_factorial(1)				# 1 = 1!
+reverse_factorial(122)				# 122 NONE
