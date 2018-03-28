e43f15aa7fa0011421ab5f2172a2992e10647e51
Alexandre Bradley <alexandre.bradley@shopify.com>
2018-03-28 10:48:05 -0400

    turn into class, for incoming rspec tests

--- 355-e/crypto.rb
+++ 355-e/crypto.rb
@@ -1,44 +1,47 @@
 # https://www.reddit.com/r/dailyprogrammer/comments/879u8b/20180326_challenge_355_easy_alphabet_cipher/
+class Crypto
+  def encrypt(input)
+    puts crypt(input,"encrypt")
+  end
 
-def encrypt(input)
-  puts crypt(input,"encrypt")
-end
+  def decrypt(input)
+    puts crypt(input,"decrypt")
+  end
 
-def decrypt(input)
-  puts crypt(input,"decrypt")
-end
+  def crypt(input, operation)
+    key, text = input.split()
+    result = ""
 
-def crypt(input, operation)
-  key, text = input.split()
-  result = ""
+    for i in 0..text.length-1
+      text_ord = text[i].ord
+      key_ord = key[i%key.length].ord - 97
+      result_ord = ""
 
-  for i in 0..text.length-1
-    text_ord = text[i].ord
-    key_ord = key[i%key.length].ord - 97
-    result_ord = ""
+      if operation == "encrypt"
+        result_ord = text_ord + key_ord
+        result_ord -= 26 if result_ord > 122
+      elsif operation == "decrypt"
+        result_ord = text_ord - key_ord
+        result_ord += 26 if result_ord < 97
+      end
 
-    if operation == "encrypt"
-      result_ord = text_ord + key_ord
-      result_ord -= 26 if result_ord > 122
-    elsif operation == "decrypt"
-      result_ord = text_ord - key_ord
-      result_ord += 26 if result_ord < 97
+      result += result_ord.chr
+
     end
 
-    result += result_ord.chr
+    result
 
   end
 
-  result
-
 end
 
-encrypt("snitch thepackagehasbeendelivered")
+crypto = Crypto.new
+crypto.encrypt("snitch thepackagehasbeendelivered")
 puts
-encrypt("bond theredfoxtrotsquietlyatmidnight")
-encrypt("train murderontheorientexpress")
-encrypt("garden themolessnuckintothegardenlastnight")
+crypto.encrypt("bond theredfoxtrotsquietlyatmidnight")
+crypto.encrypt("train murderontheorientexpress")
+crypto.encrypt("garden themolessnuckintothegardenlastnight")
 puts
-decrypt("cloak klatrgafedvtssdwywcyty")
-decrypt("python pjphmfamhrcaifxifvvfmzwqtmyswst")
-decrypt("moore rcfpsgfspiecbcc")
+crypto.decrypt("cloak klatrgafedvtssdwywcyty")
+crypto.decrypt("python pjphmfamhrcaifxifvvfmzwqtmyswst")
+crypto.decrypt("moore rcfpsgfspiecbcc")
